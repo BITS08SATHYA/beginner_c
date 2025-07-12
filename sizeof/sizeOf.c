d5a3b93e9570b5ee448cae8f5bb9a1491b2bb8ad
Sathya <neelasathya06@gmail.com>
2025-07-12 19:08:39 -0400

    sizeof exercise impl

---
+++ sizeof/sizeOf.c
@@ -0,0 +1,25 @@
+//
+// Created by sathipa on 12/7/25.
+//
+#include <stdio.h>
+
+void main(){
+
+    double x = 9.9;
+    char xx =
+
+//    printf("size = %lu\n", sizeof(double));
+    printf("size = %lu\n", sizeof(char));
+//    printf("size = %lu\n", sizeof(int));
+//    printf("size = %lu\n", sizeof(float));
+    printf("size = %lu\n", sizeof('a'));
+    printf("size = %lu\n", sizeof("a"));
+
+    int grade = 80;
+    double grade1 = 79.9;
+
+    int grade11 = 80; int grade22 = 80;
+    printf("Size of variable %lu\n", sizeof(grade11) + sizeof(grade22));
+
+
+}
