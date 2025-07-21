1c4a1f5a6bae329ade8cbaac297431b262a2b11a
Sathya <neelasathya06@gmail.com>
2025-07-20 22:08:47 -0400

    malloc and calloc impl

---
+++ 07-DMA/calloc/calloc.c
@@ -0,0 +1,13 @@
+//
+// Created by sathipa on 20/7/25.
+//
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int *a;
+    int arraySize;
+    scanf("%d", &arraySize);
+    a = (int*)calloc(5, sizeof(int));
+    return 0;
+}
