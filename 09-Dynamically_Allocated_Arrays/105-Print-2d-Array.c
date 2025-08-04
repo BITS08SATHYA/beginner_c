44d135516b74000931e44217985d77a5a7824628
Sathya <neelasathya06@gmail.com>
2025-08-03 20:25:25 -0400

    print 2D array impl

---
+++ 09-Dynamically_Allocated_Arrays/105-Print-2d-Array.c
@@ -0,0 +1,26 @@
+//
+// Created by sathipa on 3/8/25.
+//
+#include <stdio.h>
+#include <malloc.h>
+
+void print2DDynamicMatrix(int **a, int rows, int columns){
+
+    int i,j;
+    for(int i=0;i<rows;i++){
+        for(int j=0;j<columns;j++){
+            printf("%d", a[i][j]);
+        }
+        printf("\n");
+    }
+
+
+}
+
+int main(){
+//    create matrix
+//    Assign values
+//   Print Matrix
+
+
+}
